d23bba7214b88e65c40f03ee0214391f51853933
andrinja <andra.zeberga@gmail.com>
2020-01-07 21:08:53 +0100

    Fix typo in component \n Remove display name rule in eslint

--- components/highlight-cards/RitualHighlightCards.js
+++ components/highlight-cards/RitualHighlightCards.js
@@ -1,10 +1,10 @@
-import RitualHighLightCard from '../highlight-card/RitualHighLightCard';
-import HighlightCardItems from './style';
+import RitualHighlightCard from "../highlight-card/RitualHighlightCard";
+import HighlightCardItems from "./style";
 
-export default({highlights}) => (
-   <HighlightCardItems>
+export default ({ highlights }) => (
+  <HighlightCardItems>
     {highlights.map(highlight => (
-        <RitualHighLightCard key={highlight.label} {...highlight}/>
+      <RitualHighlightCard key={highlight.label} {...highlight} />
     ))}
-   </HighlightCardItems> 
-)
+  </HighlightCardItems>
+);
