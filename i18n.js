dfe98b5a6666fbefdda9183bfcd1c5aa1402bd87
andrinja <andra.zeberga@gmail.com>
2020-02-10 09:44:32 +0100

    ru removed

--- i18n.js
+++ i18n.js
@@ -2,17 +2,14 @@ const NextI18Next = require("next-i18next").default;
 
 const localeSubpathVariations = {
   none: {},
-  foreign: {
-    ru: "ru"
-  },
+  foreign: {},
   all: {
-    lv: "lv",
-    ru: "ru"
+    lv: "lv"
   }
 };
 
 module.exports = new NextI18Next({
   defaultLanguage: "lv",
-  otherLanguages: ["ru"],
+  otherLanguages: [],
   localeSubpaths: localeSubpathVariations.foreign
 });
