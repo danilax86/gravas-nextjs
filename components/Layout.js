8a0d1d6cceac2f4c4fcf1bc642b2aeaa5c7d2727
andrinja <andra.zeberga@gmail.com>
2020-03-03 21:57:36 +0100

    add meta

--- components/Layout.js
+++ components/Layout.js
@@ -31,7 +31,17 @@ const Layout = ({ children, title, description }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
         />
+        <meta property="title" content={title || ""}></meta>
         <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="pirts rituals, naktsmitnes, aktiva atputa, pirts rituali, kazu svinibas, svinibu telpas, viesu majas ire"
+        />
+        <meta name="url" content="https://www.gravas.lv/"></meta>
+        <meta
+          property="image"
+          content="https://www.gravas.lv/static/home/gravas-atputa-bauska.jpg?webp?lqip"
+        ></meta>
       </Head>
       <Navbar />
       <Container>{children}</Container>
