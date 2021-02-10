262637b353de85aa0db5e5f9a206690f08c67e7e
jedondfds <jekabs.donis@gmail.com>
2021-02-10 19:48:50 +0200

    upgrade next to 9.5

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -10,11 +10,12 @@ import ArrowDownIcon from "../../components/icons/ArrowDown";
 const FALLBACK_LANGUAGE = "lv";
 
 const LanguageSelect = () => {
+
   return (
     <SelectItems>
       <ArrowDownIcon className="icon" style={{ fontSize: "24px" }} />
       <select
-        defaultValue={i18n.language || FALLBACK_LANGUAGE}
+        value={i18n.language}
         onChange={event => {
           i18n.changeLanguage(event.target.value);
         }}
